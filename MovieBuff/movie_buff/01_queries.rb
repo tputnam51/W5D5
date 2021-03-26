a0cc8296ace11707356bdfba00909bf280e2fd28
Travis <tputnam8170@gmail.com>
2021-03-26 14:10:39 -0400

    re_commit

--- MovieBuff/movie_buff/01_queries.rb
+++ MovieBuff/movie_buff/01_queries.rb
@@ -23,7 +23,7 @@ def harrison_ford
   # Find the id and title of all movies in which Harrison Ford
   # appeared but not as a lead actor
 
-  Movie.select(:id, :title).joins(:actors).joins(:castings).where(actors: {name: 'Harrison Ford'} AND castings: {'ORD != 1'})
+  Movie.select(:id, :title).joins(:actors).joins(:castings).where(actors: {name: 'Harrison Ford'}, castings: {'ORD != 1'})
 
 end
 
