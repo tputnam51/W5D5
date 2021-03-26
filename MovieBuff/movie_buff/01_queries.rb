f8fe058197cde52558829cad2f941715c7e6deb1
Travis <tputnam8170@gmail.com>
2021-03-26 14:06:06 -0400

    01_queries part 1&2

--- MovieBuff/movie_buff/01_queries.rb
+++ MovieBuff/movie_buff/01_queries.rb
@@ -7,6 +7,8 @@ def it_was_ok
   #
   # Find the id, title, and score of all movies with scores between 2 and 3
 
+  Movie.select(:id, :title, :score).where(score: (2..3))
+
 end
 
 def harrison_ford
@@ -20,6 +22,8 @@ def harrison_ford
   #
   # Find the id and title of all movies in which Harrison Ford
   # appeared but not as a lead actor
+
+  Movie.select(:id, :title).joins(:actors).joins(:castings).where(actors: {name: 'Harrison Ford'} AND castings: {'ORD != 1'})
 
 end
 
