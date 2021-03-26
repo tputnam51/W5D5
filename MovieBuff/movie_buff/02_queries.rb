cc5e1aa34f211333da33e0e1037e0af03ddf4c43
chrisweeting <chrisweeting@gmail.com>
2021-03-26 16:07:25 -0400

    Vanity_projects

--- MovieBuff/movie_buff/02_queries.rb
+++ MovieBuff/movie_buff/02_queries.rb
@@ -13,7 +13,7 @@ end
 def cast_list(title)
   # List all the actors for a particular movie, given the title.
   # Sort the results by starring order (ord). Show the actor id and name.
-
+  Actor.joins(:movies).where(movies: {title: title}).order('castings.ord')
 end
 
 def vanity_projects
@@ -22,7 +22,7 @@ def vanity_projects
   # Show the movie id and title and director's name.
 
   # Note: Directors appear in the 'actors' table.
-
+  Movie.select(:id, :title, :name).joins(:actors).where("director_id = actor_id").where(castings: {ord: 1})
 end
 
 def most_supportive
