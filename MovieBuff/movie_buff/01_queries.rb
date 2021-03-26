e7493426e72669ae303281209fa243e1f53e36cf
chrisweeting <chrisweeting@gmail.com>
2021-03-26 15:10:29 -0400

    Eighties B movies

--- MovieBuff/movie_buff/01_queries.rb
+++ MovieBuff/movie_buff/01_queries.rb
@@ -56,7 +56,7 @@ def directed_by_one_of(them)
   # Movie.where(yr: years)
   #
   # Find the id and title of all the movies directed by one of 'them'.
-  Movie.select(:id, :title).where() 
+  Movie.select(:id, :title).joins(:director).where(actors: {name: them}) 
 end
 
 def movie_names_before_1940
@@ -70,5 +70,5 @@ def movie_names_before_1940
   # improve performace for larger queries.
   #
   # Use pluck to find the title of all movies made before 1940.
-
+  Movie.where("yr < 1940").pluck(:title)
 end
