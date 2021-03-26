182fee685941419befb1f964eafc55894305e2ad
Travis <tputnam8170@gmail.com>
2021-03-26 16:58:11 -0400

    whar_was_that_one

--- MovieBuff/movie_buff/03_queries.rb
+++ MovieBuff/movie_buff/03_queries.rb
@@ -2,6 +2,7 @@ def what_was_that_one_with(those_actors)
   # Find the movies starring all `those_actors` (an array of actor names).
   # Show each movie's title and id.
 
+  Movie.select(:id, :title).joins(:actors).where(actors: {name: those_actors}).group(:id).having('COUNT(actor.id) = those_actors.length')
 end
 
 def golden_age
